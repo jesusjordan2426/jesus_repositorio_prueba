a5a0b218966dffced84ffb7f32591b9366edff38
jesusjordan2426 <jesus.jordan.vargas@colexio-karbo.com>
2024-12-10 12:20:22 +0100

    Primer commit: Añadido proyectopruebagit.java

---
+++ src/main/java/com/mycompany/proyectopruebagit/Proyectopruebagit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectopruebagit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyectopruebagit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
